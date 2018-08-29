4c942e30ab3d925facbed1c333c8034386327b1b
liuhua <411981379@qq.com>
2018-08-29 10:24:09 +0800

    .

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,17 @@ var connection = mysql.createConnection({
   database : 'wisnuc'
 });
 
-connection.connect();
+connection.connect(err => {
+  if (err) console.log(err)
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  let beginTime = (new Date()).getTime()
   connection.query('SELECT * FROM user', function (error, results, fields) {
+    let endTime = (new Date()).getTime()
     if (error) throw error;
-    res.status(200).json(results)
+    res.status(200).json(endTime-beginTime)
   })
 });
 
