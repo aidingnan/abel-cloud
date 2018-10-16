f7eedb7a9cfdc0aef11c8eb0e6fa42ffe7b9a824
liuhua <411981379@qq.com>
2018-10-16 17:54:07 +0800

    adjust in pipe

---
+++ service/base.js
@@ -0,0 +1,127 @@
+const EventEmitter = require('events').EventEmitter
+const uuid = require('uuid')
+const debug = require('debug')('app:store')
+
+class State {
+  constructor(ctx, ...args) {
+    this.ctx = ctx
+    this.ctx.state = this
+    debug(`${this.ctx.jobId} enter state : ${this.constructor.name}`)
+    this.enter(...args)
+  }
+
+  setState(NextState, ...args) {
+    this.exit()
+    new NextState(this.ctx, ...args)
+  }
+
+  enter() {}
+
+  exit() {}
+}
+
+class Notice extends State {
+  constructor(ctx) {
+    super(ctx)
+  }
+
+  async enter() {
+    try {
+      let sn = this.ctx.req.params.sn
+      let topic = `cloud/${sn}/pipe`
+      let qos = 1
+      let payload = JSON.stringify(this.ctx.manifest)
+      let obj = { topic, qos, payload}
+      publishAsync(obj)
+      this.setState(Pending)
+    } catch (e) { this.setState(Error, e, 500) }
+    
+  }
+}
+
+/**
+ * 等待： 等待NAS请求
+ */
+class Pending extends State {
+  constructor(ctx) {
+    super(ctx)
+  }
+}
+
+/**
+ * 完成： 获取NAS返回数据
+ */
+class Finish extends State {
+  constructor(ctx, ...args) {
+    super(ctx, ...args)
+  }
+
+  enter(data) {
+    this.ctx.success(data)
+  }
+}
+
+/**
+ * 错误： NAS返回错误或执行出错
+ */
+class Err extends State {
+  constructor(ctx, ...args) {
+    super(ctx, ...args)
+  }
+
+  enter(error, code) {
+    this.ctx.error(error, code)
+  }
+}
+
+/**
+ * Server
+ */
+
+class Server extends EventEmitter {
+
+  constructor(req, res, ctx, Init) {
+    super()
+    this.req = req
+    this.res = res
+    this.ctx = ctx
+    this.manifest = null
+    this.timer = Date.now() + 150 * 1000
+    this.jobId = uuid.v4()
+    this.state = null
+    new Init(this)
+    
+    this.req.on('error', err => this.error(err))
+    this.req.on('close', this.abort)
+  }
+
+  isTimeOut() {
+    if (Date.now() > this.timer) {
+      let e = new E.PipeResponseTimeout()
+      this.error(e)
+      return true
+    }
+    return false
+  }
+  
+  finished() {
+    return this.res.finished
+  }
+
+  success(data) {
+    if (this.finished()) return
+    this.res.success(data)
+  }
+
+  error(err, code) {
+    if (this.finished()) return
+    this.res.error(err, code)
+  }
+
+  abort() {
+    this.res.finished = true
+  }
+}
+
+
+module.exports = { State, Pending, Notice, Finish, Err, Server }
--- service/transformJson.js
+++ service/transformJson.js
@@ -2,34 +2,17 @@
  * @Author: harry.liu 
  * @Date: 2018-10-10 16:49:15 
  * @Last Modified by: harry.liu
- * @Last Modified time: 2018-10-15 18:23:33
+ * @Last Modified time: 2018-10-16 17:48:01
  */
-
-const EventEmitter = require('events').EventEmitter
-const uuid = require('uuid')
+const debug = require('debug')('app:store')
 const E = require('../lib/error')
+const { State, Notice, Finish, Err, Server } = require('./base')
 
 /**
- * 状态机基类
+ * 状态机
  * server state: init -> notice -> pending -> finish
  */
-class State {
-  constructor(ctx, ...args) {
-    this.ctx = ctx
-    this.ctx.state = this
-    console.log(`${this.ctx.jobId} enter state : ${this.constructor.name}`)
-    this.enter(...args)
-  }
 
-  setState(NextState, ...args) {
-    this.exit()
-    new NextState(this.ctx, ...args)
-  }
-
-  enter() {}
-
-  exit() {}
-}
 
 /**
  * 初始化：获取参数
@@ -48,6 +31,8 @@ class Init extends State {
     if (server.req.method == 'GET') body = server.req.query
     else if (server.req.method == 'POST') body = server.req.body
     let { method, resource } = body
+    delete body.method
+    delete body.resource
 
     this.ctx.manifest = {
       method, resource, body,
@@ -59,119 +44,12 @@ class Init extends State {
   }
 }
 
-/**
- * 通知：发布消息
- */
-class Notice extends State {
-  constructor(ctx) {
-    super(ctx)
-    this.name = 'notice'
-  }
-
-  async enter() {
-    try {
-      let sn = this.ctx.req.params.sn
-      let topic = `cloud/${sn}/pipe`
-      let qos = 1
-      let payload = JSON.stringify(this.ctx.manifest)
-      let obj = { topic, qos, payload}
-      publishAsync(obj)
-      this.setState(Pending)
-    } catch (e) { console.log(e) }
-    
-  }
-}
-
-/**
- * 等待： 等待NAS请求
- */
-class Pending extends State {
-  constructor(ctx) {
-    super(ctx)
-  }
-}
-
-/**
- * 完成： 获取NAS返回数据
- */
-class Finish extends State {
-  constructor(ctx, ...args) {
-    super(ctx, ...args)
-  }
-
-  enter(data) {
-    this.ctx.success(data)
-  }
-}
-
-/**
- * 错误： NAS返回错误或执行出错
- */
-class Error extends State {
-  constructor(ctx, ...args) {
-    super(ctx, ...args)
-  }
-
-  enter(error, code) {
-    this.ctx.error(error, code)
-    this.ctx.ctx.close(this.ctx.jobId)
-  }
-}
-
-/**
- * 客户端请求
- */
-class Server extends EventEmitter {
-  constructor(req, res, ctx) {
-    super()
-    this.req = req
-    this.res = res
-    this.ctx = ctx
-    this.manifest = null
-    this.timer = Date.now() + 15*1000
-    this.jobId = uuid.v4()
-    this.state = null
-    this.req.on('error', err => this.error(err))
-    this.req.on('close', this.abort)
-    
-    new Init(this)
-  }
-
-  isTimeOut() {
-    if (Date.now() > this.timer) {
-      let e = new E.PipeResponseTimeout()
-      this.error(e)
-      return true
-    }
-    return false
-  }
-
-  // 获取res状态
-  finished() {
-    return this.res.finished
-  }
-
-  success(data) {
-    if (this.finished()) return
-    this.res.success(data)
-  }
-
-  error(err, code) {
-    if (this.finished()) return
-    this.res.error(err, code)
-  }
-
-  abort() {
-    this.res.finished = true
-  }
-}
 
 /**
  * server 管理容器
  */
-class TransformJson extends EventEmitter {
+class TransformJson {
   constructor(limit) {
-    super()
     this.limit = limit || 1024
     this.map = new Map()
   }
@@ -187,36 +65,36 @@ class TransformJson extends EventEmitter {
 
   // 创建客户端请求
   createServer(req, res) {
-    console.log(this.map.size)
     this.schedule()
-    console.log(this.map.size)
     if (this.map.size > this.limit) throw new E.PipeTooMuchTask()
-    new Server(req, res, this)
+    debug(this.map.size)
+    new Server(req, res, this, Init)
   }
 
   // 对客户端请求进行返回
   request(req, res) {
     let { jobId } = req.params
     let server = this.map.get(jobId)
+    // 任务不存在
     if (!server) return res.error(new E.TransformJsonQueueNoServer(), 403, false)
 
     // 超时
     if (server.isTimeOut()) {
       let e = new E.PipeResponseTimeout()
-      server.state.setState(Error, e)
+      server.state.setState(Err, e)
       return res.error(e)
     }
     // 重复发送或者客户端撤销请求
     if (server.finished()) {
       let e = new E.PipeResponseHaveFinished()
-      server.state.setState(Error, e)
+      server.state.setState(Err, e)
       return res.error(e)
     }
 
     let responseError = req.body.error
     if (responseError) {
       // 将station错误返回至客户端
-      server.state.setState(Error, responseError.message, responseError.code)
+      server.state.setState(Err, responseError.message, responseError.code)
     }
     else {
       // 将station数据返回至客户端
