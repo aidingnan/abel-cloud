d9fe542cec2c511b76de29d051b9781b4b14372f
liuhua <411981379@qq.com>
2018-12-13 19:27:37 +0800

    .

--- routes/client/v1/index.js
+++ routes/client/v1/index.js
@@ -3,12 +3,13 @@ const router = express.Router()
 const { cAuth } = require('../../../middlewares/jwt')
 var timeout = require('connect-timeout')
 
-router.use('/station', require('./station'))
+
 // router.use(timeout('15s'))
 router.get('/', (req, res) => res.success({}))
 router.use('/user', require('./user'))
 router.use('/wechat', require('./wechat'))
 router.use('*', cAuth)
+router.use('/station', require('./station'))
 
 
 
