273850138dfac8d38e048763839770e393c5c490
liuhua <411981379@qq.com>
2019-01-29 14:19:50 +0800

    .

--- routes/server/v1/index.js
+++ routes/server/v1/index.js
@@ -6,7 +6,7 @@ const { sAuth } = require('../../../middlewares/jwt')
 router.use(logger(':remote-addr [:date[clf]] ":method :url :status :response-time ms', {
     skip: (req, res) => { return res.statusCode == 200 }
 }))
-// router.use('*', sAuth)
+router.use('*', sAuth)
 router.use('/station', require('./station'))
 
 module.exports = router
