d67077b635f9a2c18c2e6e564a13b2bed1b8f2bb
liuhua <411981379@qq.com>
2019-02-14 14:55:53 +0800

    add CORS

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ app.use(async (req, res, next) => {
   }
 })
 
+app.all('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1')
+      //这段仅仅为了方便返回json而已
+  res.header("Content-Type", "application/json;charset=utf-8");
+  if(req.method == 'OPTIONS') {
+      //让options请求快速返回
+      res.sendStatus(200); 
+  } else { 
+      next(); 
+  }
+});
+
 app.use(express.json({ limit: '5mb', extended: false }))
 app.use(cookieParser())
 
