eb93bfa046e5fc679673d5069d99fd2a437fd21c
liuhua <411981379@qq.com>
2019-02-22 18:24:27 +0800

    .

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(async (req, res, next) => {
 
 app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With, Authorization,Content-Type");
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By", ' 3.2.1')
       //这段仅仅为了方便返回json而已
