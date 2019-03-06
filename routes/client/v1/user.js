17ba7327d8af7bf6fb62bb6adb297d34c70051d4
liuhua <411981379@qq.com>
2019-03-06 17:06:24 +0800

    .

--- routes/client/v1/user.js
+++ routes/client/v1/user.js
@@ -2,7 +2,7 @@
  * @Author: harry.liu 
  * @Date: 2018-09-05 13:25:16 
  * @Last Modified by: harry.liu
- * @Last Modified time: 2019-03-06 17:05:15
+ * @Last Modified time: 2019-03-06 17:06:22
  */
 const express = require('express')
 const router = express.Router()
@@ -190,9 +190,11 @@ router.post('/wechat', joiValidator({
   }
 }), cAuth, async (req, res) => {
   try {
+    console.log('123')
     let { id, avatarUrl } = req.auth
     let { code, type } = req.body
     let result = await userService.addWechat(req.db, id, code, type, avatarUrl)
+    console.log('456')
     res.success(result)
   } catch (e) {
     res.error(e)
