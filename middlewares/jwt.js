5f76b900e4016b4c39d3223b105346d6e3f012be
liuhua <411981379@qq.com>
2019-03-07 16:27:37 +0800

    .

--- middlewares/jwt.js
+++ middlewares/jwt.js
@@ -96,7 +96,6 @@ module.exports = {
   async sAuth(req, res, next) {
     try {
       const aut = req.headers.authorization
-      console.log('aut is :', aut)
       if (!aut) throw new Error()
       
       // decode
