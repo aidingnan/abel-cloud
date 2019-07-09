becbfdcc1352e9b865e9e2c1a37454f46787f8fc
root <root@DESKTOP-TIN7IA7.localdomain>
2019-07-09 15:06:51 +0800

    add upgrade query

--- routes/server/v1/index.js
+++ routes/server/v1/index.js
@@ -2,10 +2,20 @@ const express = require('express')
 const logger = require('morgan')
 const router = express.Router()
 const { sAuth } = require('../../../middlewares/jwt')
+const stationService = require('../../../service/stationService')
 
 router.use(logger(':remote-addr [:date[clf]] ":method :url :status :response-time ms', {
-    skip: (req, res) => { return res.statusCode == 200 }
+  skip: (req, res) => { return res.statusCode == 200 }
 }))
+
+router.get('/station/upgrade', async (req, res) => {
+  try {
+    let result = await stationService.getStationUpgrade(req.db)
+    res.success(result)
+  } catch (error) {
+    res.error
+  }
+})
 router.use('*', sAuth)
 router.use('/station', require('./station'))
 
